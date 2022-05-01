3faaa5f221f114171b73b1ded3272a4d2f43bfed
ItsMCB <33915290+ItsMCB@users.noreply.github.com>
2022-04-30 20:27:55 -0500

    Added more user options to dl.sh

--- dl.sh
+++ dl.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
+# Styling
+iconHarddrive='\U1F5B4'
+iconFile='\U1F5CE'
+iconCamera='\U1F4F9'
+iconSpeaker='\U1F50A'
+
+# Default formats
+typeVideo="video"
+typeAudio="audio"
+commonFormat="%(title)s.%(ext)s"
+
 # Set default variables
+type="${typeVideo}"
 videoExt="mp4"
-audioExt="m4a"
+audioExt="mp3"
 downloadLocation="~/Desktop/"
 outputFormat="%(title)s.%(ext)s"
+includeMetadata="true"
 extraMetadataFlags='--embed-metadata --add-metadata --xattrs --embed-thumbnail'
+covert=""
 
 # Functions
-branding() {
-    echo "===== YT-DLP DOWNLOAD UTILITY - ${USERNAME} ====="
-}
 
 home() {
-    clear
-    branding
-    echo "1.) Download Video | V"
-    echo "2.) Download Audio | A"
-    echo "4.) Quit Program | Q"
+    prompt "What would you like to do?" "Download Video [V]\nDownload Audio [A]\nQuit [Q]"
     read -p "" ans;
     case $ans in
         v|v)
-            downloadVideo;;
+            type="${typeVideo}";;
         a|A)
-            downloadAudio;;
+            type="${typeAudio}";;
         *)
-            clear
             exit;;
     esac
+    dl
 }
 
-getVideoExt() {
-    echo "Select target video format."
-    echo "1.) MP4 | [Enter]"
-    echo "2.) Other | *"
+askVideoExt() {
+    prompt "What target video extension?" "MP4 [Enter]\nOther [Input]"
     read -p "" ans;
     case $ans in
         ""|\n)
@@ -41,36 +46,103 @@ getVideoExt() {
         *)
             videoExt=$ans;;
     esac
-    echo "Using video extension '${videoExt}'..."
 }
 
-getAudoExt() {
-    echo "Select target audio format."
-    echo "1.) M4A | [Enter]"
-    echo "2.) Other | *"
+askAudioExt() {
+    prompt "What target audio extension?" "M4A [1]\nOther [Input]"
     read -p "" ans;
     case $ans in
-        ""|\n)
+        1|"1")
             audioExt="m4a";;
         *)
             audioExt=$ans;;
     esac
-    echo "Using audio extension '${audioExt}'..."
+    prompt "Convert to another format?" "FLAC (lossless) [1]\nSkip/No [Enter]"
+    read -p "" ans;
+    case $ans in
+        1|"1")
+            covert="-x --audio-format flac";;
+        *)
+            ;;
+    esac
 }
 
-
-downloadVideo() {
-    getVideoExt
-    echo "URL of video to download:"
+askMetadata() {
+    prompt "Include metadata?" "Yes [y]\nNo [n]"
     read -p "" ans;
-    echo "Downloading best ${videoExt} format available. If there are none, find the best in another format."
-    yt-dlp -f "bestvideo[ext=${videoExt}]+bestaudio[ext=${audioExt}]/best[ext=${videoExt}]/best" -o ${downloadLocation}${outputFormat} ${extraMetadataFlags} $ans
+    case $ans in
+        y|Y|"y"|"Y")
+            includeMetadata="true";;
+        *)
+            includeMetadata="false";;
+    esac
 }
 
-downloadAudio() {
-    echo "Please provide a URL to be converted into an mp3"
+askDownloadLocation() {
+    prompt "Where should output file go?" "Desktop [1]\nOther [Input]"
     read -p "" ans;
-    yt-dlp -f "bestaudio[ext=${audioExt}]/best" -o ${downloadLocation}${outputFormat} ${extraMetadataFlags} $ans
+    case $ans in
+        1)
+            downloadLocation="~/Desktop/";;
+        *)
+            downloadLocation=$ans;;
+    esac
+}
+
+askOutputFormat() {
+    prompt "How should the output file be formatted?" "${commonFormat} [1]\nOther [Input]"
+    read -p "" ans;
+    case $ans in
+        1)
+            outputFormat="${commonFormat}";;
+        *)
+            outputFormat="$ans";;
+    esac
+}
+
+dl() {
+    # Create final command args variable that will be appended to
+    download=""
+    # Check if type audio or video
+    if [ "${type}" = "${typeVideo}" ]; then
+        askVideoExt
+        download="bestvideo[ext=${videoExt}]+bestaudio[ext=${audioExt}]/best[ext=${videoExt}]/best"
+    else
+        askAudioExt
+        download="bestaudio[ext=${audioExt}]/best"
+    fi
+    # Check download location and output format
+    askDownloadLocation
+    askOutputFormat
+    download="${download} -o ${downloadLocation}${outputFormat}"
+    # Check if should include file metadata
+    askMetadata
+    if [ "${includeMetadata}" = "true" ]; then
+        download="${download} ${extraMetadataFlags}"
+    fi
+
+    # Finally ask for content url
+    printStatus
+    prompt "What's the content URL?" "* [Input]"
+    read -p "" url;
+    download="${download} ${covert} ${url}"
+
+    echo "Running the following command: 'yt-dlp -f ${download}'"
+    yt-dlp -f ${download}
+}
+
+prompt() {
+    printStatus
+    echo ""
+    echo "$1"
+    echo ""
+    echo "Options:"
+    echo -e "$2"
+}
+
+printStatus() {
+    clear
+    echo -e "====== - YT-DLP Utility - ${iconHarddrive} ${downloadLocation} - ${iconFile} ${iconCamera} ${videoExt} - ${iconFile} ${iconSpeaker} ${audioExt} -======"
 }
 
 home
